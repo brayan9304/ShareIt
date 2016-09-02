2cb69a6b32d2161c58c5bff6b4f6934518c5ec95
jaime <jaime.jimenezisi@gmail.com>
2016-09-01 22:01:38 -0500

    Creación fragment de busqueda y mejora de navegation drawer

--- app/src/main/java/co/edu/udea/compumovil/gr06/shareit/UI/MainActivity.java
+++ app/src/main/java/co/edu/udea/compumovil/gr06/shareit/UI/MainActivity.java
@@ -1,11 +1,10 @@
 package co.edu.udea.compumovil.gr06.shareit.UI;
 
-import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
