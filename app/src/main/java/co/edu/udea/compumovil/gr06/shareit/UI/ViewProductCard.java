fe0d42a7d3216bc5f2aa1d04df0c4a902604c635
Brayan <brayan-416@hotmail.com>
2016-11-17 23:22:24 -0500

    Botones de ver localizacion y ver comentarios

--- app/src/main/java/co/edu/udea/compumovil/gr06/shareit/UI/ViewProductCard.java
+++ app/src/main/java/co/edu/udea/compumovil/gr06/shareit/UI/ViewProductCard.java
@@ -2,14 +2,34 @@ package co.edu.udea.compumovil.gr06.shareit.UI;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import co.edu.udea.compumovil.gr06.shareit.R;
 
 public class ViewProductCard extends AppCompatActivity {
-
+    private Button verComentarios;
+    private Button verLocalizacion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_product_card);
+        verComentarios = (Button)findViewById(R.id.buttonComentarios);
+        verLocalizacion = (Button)findViewById(R.id.buttonLocalization);
+
+        verComentarios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Crear intent
+            }
+        });
+
+        verLocalizacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Crear intent
+            }
+        });
     }
 }
